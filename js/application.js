42800fa9cbd0a2747c89fe806a95dacea502af97
lou <louiscuny@gmail.com>
2011-04-15 10:00:58 +0200

    more demos

--- js/application.js
+++ js/application.js
@@ -1,9 +1,32 @@
 (function($){
-
   $(function(){
     $('.multiselect').multiSelect();
+    
+    $('#callbackCountries').multiSelect({
+      afterSelect: function(value, text){
+        alert('Select element\nvalue: '+value+'\ntext: '+text);
+      },
+      afterDeselect: function(value, text){
+        alert('Deselect element\nvalue: '+value+'\ntext: '+text);
+      }
+    });
+
+    $('#selectFR').click(function(){
+      $('#outsideCountries').multiSelect('select', 'fr');
+      return false;
+    });
+    
+    
+    $('#deselectFR').click(function(){
+      $('#outsideCountries').multiSelect('deselect', 'fr');
+      return false;
+    });
+    
+    $('#demos-menu li').click(function(){
+      $('#demos-menu li').removeClass('active');
+      $('#demos-content').children('div').hide();
+      $(this).addClass('active');
+      $('#demos-content .'+$(this).attr('id')).show();
+    });
   });
-
-  hljs.initHighlightingOnLoad();
-
 })(jQuery)
