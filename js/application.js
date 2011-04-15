374c6d9fe4c7925b120979a1ae25b3600b1c8e51
lou <louiscuny@gmail.com>
2011-04-15 17:23:14 +0200

    select all demo

--- js/application.js
+++ js/application.js
@@ -11,11 +11,20 @@
       }
     });
 
+    $('#selectAll').click(function(){
+      $('#outsideCountries').multiSelect('select_all');
+      return false;
+    });
+    
+    $('#deselectAll').click(function(){
+      $('#outsideCountries').multiSelect('deselect_all');
+      return false;
+    });
+
     $('#selectFR').click(function(){
       $('#outsideCountries').multiSelect('select', 'fr');
       return false;
     });
-    
     
     $('#deselectFR').click(function(){
       $('#outsideCountries').multiSelect('deselect', 'fr');
