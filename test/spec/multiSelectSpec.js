6c95d17e7286566452dd67c3fc3f6770affa35dc
Louis Cuny <louiscuny@gmail.com>
2012-07-26 16:00:11 +0200

    re-order specs + add specs for deselect method

--- test/spec/multiSelectSpec.js
+++ test/spec/multiSelectSpec.js
@@ -15,119 +15,159 @@ describe("multiSelect", function() {
     expect(select.hasClass('chainable')).toBeTruthy();
   });
 
-  describe('init without options', function(){
 
-    beforeEach(function() {
-      select.multiSelect();
-      msContainer = select.next();
-    });
+  describe('init', function(){
+    describe('without options', function(){
 
-    it('should hide the original select', function(){
-      expect(select.css('position')).toBe('absolute');
-      expect(select.css('left')).toBe('-9999px');
-    });
-
-    it('should create a container', function(){
-      expect(msContainer).toBe('div.ms-container');
-    });
-
-    it ('should create a selectable and a selection container', function(){
-      expect(msContainer).toContain('div.ms-selectable, div.ms-selection');
-    });
-
-    it ('should create a list for both selectable and selection container', function(){
-      expect(msContainer).toContain('div.ms-selectable ul.ms-list, div.ms-selection ul.ms-list');
-    });
-
-    it ('should populate the selectable list', function(){
-      expect($('.ms-selectable ul.ms-list li').length).toEqual(10);
-    });
-
-    it ('should not populate the selection list if none of the option is selected', function(){
-      expect($('.ms-selection ul.ms-list li').length).toEqual(0);
-    });
-
-  });
-
-  describe('init with pre-selected options', function(){
-
-    var selectedValues = [];
-
-    beforeEach(function() {
-      var firstOption = select.children('option').first();
-      var lastOption = select.children('option').last();
-      firstOption.prop('selected', true);
-      lastOption.prop('selected', true);
-      selectedValues.push(firstOption.val(), lastOption.val());
-      select.multiSelect();
-      msContainer = select.next();
-    });
-
-    it ('should select the pre-selected options', function(){
-      $.each(selectedValues, function(index, value){
-        expect($('.ms-selectable ul.ms-list li[ms-value="'+value+'"]')).toBe('.ms-selected');
+      beforeEach(function() {
+        select.multiSelect();
+        msContainer = select.next();
       });
-      expect($('.ms-selectable ul.ms-list li.ms-selected').length).toEqual(2);
+
+      it('should hide the original select', function(){
+        expect(select.css('position')).toBe('absolute');
+        expect(select.css('left')).toBe('-9999px');
+      });
+
+      it('should create a container', function(){
+        expect(msContainer).toBe('div.ms-container');
+      });
+
+      it ('should create a selectable and a selection container', function(){
+        expect(msContainer).toContain('div.ms-selectable, div.ms-selection');
+      });
+
+      it ('should create a list for both selectable and selection container', function(){
+        expect(msContainer).toContain('div.ms-selectable ul.ms-list, div.ms-selection ul.ms-list');
+      });
+
+      it ('should populate the selectable list', function(){
+        expect($('.ms-selectable ul.ms-list li').length).toEqual(10);
+      });
+
+      it ('should not populate the selection list if none of the option is selected', function(){
+        expect($('.ms-selection ul.ms-list li').length).toEqual(0);
+      });
+
+    });
+
+    describe('with pre-selected options', function(){
+
+      var selectedValues = [];
+
+      beforeEach(function() {
+        var firstOption = select.children('option').first();
+        var lastOption = select.children('option').last();
+        firstOption.prop('selected', true);
+        lastOption.prop('selected', true);
+        selectedValues.push(firstOption.val(), lastOption.val());
+        select.multiSelect();
+        msContainer = select.next();
+      });
+
+      it ('should select the pre-selected options', function(){
+        $.each(selectedValues, function(index, value){
+          expect($('.ms-selectable ul.ms-list li[ms-value="'+value+'"]')).toBe('.ms-selected');
+        });
+        expect($('.ms-selectable ul.ms-list li.ms-selected').length).toEqual(2);
+      });
+    });
+
+    describe('with keepOrder option activated', function(){
+      beforeEach(function() {
+        $('#multi-select').multiSelect({ keepOrder: true });
+        msContainer = select.next();
+        firstItem = $('.ms-selectable ul.ms-list li').first()
+        lastItem = $('.ms-selectable ul.ms-list li').last();
+        lastItem.trigger('click');
+        firstItem.trigger('click');
+      });
+
+      it('should keep order on selection list', function(){
+        expect($('.ms-selection li', msContainer).first().attr('ms-value')).toBe('value1');
+        expect($('.ms-selection li', msContainer).last().attr('ms-value')).toBe('value10');
+      });
     });
   });
 
-  describe('init with keepOrder option activated', function(){
-    beforeEach(function() {
-      $('#multi-select').multiSelect({ keepOrder: true });
-      msContainer = select.next();
-      firstItem = $('.ms-selectable ul.ms-list li').first()
-      lastItem = $('.ms-selectable ul.ms-list li').last();
-      lastItem.trigger('click');
-      firstItem.trigger('click');
-    });
+  describe('select', function(){
+    describe("on click", function(){
+      var clickedItem;
 
-    it('should keep order on selection list', function(){
-      expect($('.ms-selection li', msContainer).first().attr('ms-value')).toBe('value1');
-      expect($('.ms-selection li', msContainer).last().attr('ms-value')).toBe('value10');
+      beforeEach(function() {
+        $('#multi-select').multiSelect();
+        clickedItem = $('.ms-selectable ul.ms-list li').first();
+        spyOnEvent(select, 'change');
+        spyOnEvent(select, 'focus');
+        clickedItem.trigger('click');
+      });
+
+      it('should hide selected item', function(){
+        expect(clickedItem).toBeHidden();
+      });
+
+      it('should add the .ms-selected class to the selected item', function(){
+        expect(clickedItem.hasClass('ms-selected')).toBeTruthy();
+      });
+
+      it('should select corresponding option', function(){
+        expect(select.children().first()).toBeSelected();
+      });
+
+      it('should populate the selection ul with the rigth item', function(){
+        expect($('.ms-selection ul.ms-list li').first()).toBe('li.ms-elem-selected[ms-value="value1"]');
+      });
+
+      it('should trigger the original select change event', function(){
+        expect('change').toHaveBeenTriggeredOn("#multi-select");
+      });
+
+      it('should trigger the original select focus event', function(){
+        expect('focus').toHaveBeenTriggeredOn("#multi-select");
+      });
+
+      afterEach(function(){
+        select.multiSelect('deselect_all');
+      });
     });
   });
 
-  
+  describe('deselect', function(){
+    describe("on click", function(){
+      var clickedItem;
+      var correspondingSelectableItem;
 
-  describe("When selectable item is clicked", function(){
-    var clickedItem;
+      beforeEach(function() {
+        $('#multi-select').find('option').first().prop('selected', true);
+        $('#multi-select').multiSelect();
 
-    beforeEach(function() {
-      $('#multi-select').multiSelect();
-      clickedItem = $('.ms-selectable ul.ms-list li').first();
-      spyOnEvent(select, 'change');
-      spyOnEvent(select, 'focus');
-      clickedItem.trigger('click');
+        clickedItem = $('.ms-selection ul.ms-list li').first();
+        correspondingSelectableItem = $('.ms-selection ul.ms-list li').first();
+        spyOnEvent(select, 'change');
+        spyOnEvent(select, 'focus');
+        clickedItem.trigger('click');
+      });
+
+      it('should remove selected item from the selection list', function(){
+        expect($('.ms-selection ul.ms-list li').length).toEqual(0);
+      });
+
+      it('should remove the .ms-selected class to the corresponding selectable item', function(){
+        expect(correspondingSelectableItem.hasClass('ms-selected')).toBeFalsy();
+      });
+
+      it('should deselect corresponding option', function(){
+        expect(select.children().first()).not.toBeSelected();
+      });
+
+      it('should trigger the original select change event', function(){
+        expect('change').toHaveBeenTriggeredOn("#multi-select");
+      });
+
+      afterEach(function(){
+        select.multiSelect('deselect_all');
+      });
     });
-
-    it('should hide selected item', function(){
-      expect(clickedItem).toBeHidden();
-    });
-
-    it('should add the .ms-selected to the selected item', function(){
-      expect(clickedItem).toBe('.ms-selected');
-    });
-
-    it('should select corresponding option', function(){
-      expect(select.children().first()).toBeSelected();
-    });
-
-    it('should populate the selection ul with the rigth item', function(){
-      expect($('.ms-selection ul.ms-list li').first()).toBe('li.ms-elem-selected[ms-value="value1"]');
-    });
-
-    it('should trigger the original select change event', function(){
-      expect('change').toHaveBeenTriggeredOn("#multi-select");
-    });
-
-    it('should trigger the original select focus event', function(){
-      expect('focus').toHaveBeenTriggeredOn("#multi-select");
-    });
-
-    afterEach(function(){
-      select.multiSelect('deselect_all');
-    });
-
   });
 
   afterEach(function () {
