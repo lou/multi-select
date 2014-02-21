e7633a043a9677c23e3a5228fceeac54be346897
lou <louiscuny@gmail.com>
2014-02-21 16:05:14 +0100

    fix sanitize method on tests

--- test/spec/SpecHelper.js
+++ test/spec/SpecHelper.js
@@ -14,6 +14,13 @@ afterEach(function () {
 });
 
 sanitize = function(value){
-  reg = new RegExp("\\W+", 'gi');
-  return value.replace(reg, '_');
+  var hash = 0, i, char;
+  if (value.length == 0) return hash;
+  var ls = 0;
+  for (i = 0, ls = value.length; i < ls; i++) {
+    char  = value.charCodeAt(i);
+    hash  = ((hash<<5)-hash)+char;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
 }
